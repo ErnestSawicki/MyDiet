6b584518d9e147ddd44bf70192ed92dca33d8428
erreku777 <a.k.przytula@gmail.com>
2020-02-15 13:05:44 +0100

    IngredientCategory

--- src/main/java/pl/com/MyDiet/MyDiet/data/model/IngredientCategory.java
+++ src/main/java/pl/com/MyDiet/MyDiet/data/model/IngredientCategory.java
@@ -1,25 +1,21 @@
 package pl.com.MyDiet.MyDiet.data.model;
 
+
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
-@Table(name = "ingredients")
+@Table(name = "ingredient_category")
 @Getter @Setter
-public class Ingredient {
+public class IngredientCategory {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
     private String name;
-
-    @Column(name = "calories_per_100g")
-    private Long caloriesPer100gram;
-
-    @Column(name = "product_categories")
-    private List<IngredientCategory> ingredientCategories;
+    
 }
