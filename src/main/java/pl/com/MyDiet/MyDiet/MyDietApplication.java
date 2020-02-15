d95c45b382fef64fd65e65f7f0e93e799a99e630
Ernest Sawicki <ErnestSawicki94@gmail.com>
2020-02-15 09:57:47 +0100

    SpringBoot init

---
+++ src/main/java/pl/com/MyDiet/MyDiet/MyDietApplication.java
@@ -0,0 +1,13 @@
+package pl.com.MyDiet.MyDiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDietApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/MyDiet/MyDiet/MyDietApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.MyDiet.MyDiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
