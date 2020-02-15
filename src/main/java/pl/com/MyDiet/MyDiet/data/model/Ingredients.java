9badb24838600d6a7dccfb94469edcffe84e1d2b
erreku777 <a.k.przytula@gmail.com>
2020-02-15 12:04:06 +0100

    created MEAL

--- src/main/java/pl/com/MyDiet/MyDiet/data/model/Ingredients.java
+++ src/main/java/pl/com/MyDiet/MyDiet/data/model/Ingredients.java
@@ -1,4 +1,19 @@
 package pl.com.MyDiet.MyDiet.data.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ingredients")
+@Getter @Setter
 public class Ingredients {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private S
+
 }
--- src/main/java/pl/com/MyDiet/MyDiet/data/model/Meal.java
+++ src/main/java/pl/com/MyDiet/MyDiet/data/model/Meal.java
@@ -1,4 +1,32 @@
 package pl.com.MyDiet.MyDiet.data.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Meal")
+@Getter @Setter
 public class Meal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "calories")
+    private Long calories;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "ower_id")
+    private Long ownerId;
+
+    @Column(name = "reciple")
+    private String recipe;
+
+    @Column(name = "preparation_time")
+    private Long preparationTime;
+
+
 }
