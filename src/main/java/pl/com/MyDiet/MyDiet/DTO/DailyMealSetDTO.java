68cb02492f2c7ee965e20ef91d5a9b48e37beff2
Arkadiusz Przytuła <a.k.przytula@gmail.com>
2020-02-24 19:25:37 +0100

    dailySetForm created

--- src/main/java/pl/com/MyDiet/MyDiet/DTO/DailyMealSetDTO.java
+++ src/main/java/pl/com/MyDiet/MyDiet/DTO/DailyMealSetDTO.java
@@ -8,14 +8,27 @@ import pl.com.MyDiet.MyDiet.data.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 public class DailyMealSetDTO {
 
     private Long mealAmount;
-    private Long calories;
+    private Long caloriesPicked;
+    private Long calories=0L;
     private User creatorUser;
+    private boolean mealPicked=false;
     private List<SimpleMealsDTO> meals = new ArrayList<>();
+
+
+    public void countCalories(){
+        Long result =0L;
+        List<Long> collect = meals.stream().map(SimpleMealsDTO::getCalories).collect(Collectors.toList());
+        for (Long l :collect) {
+            result+=l;
+        }
+        this.calories=result;
+    }
 
     @Override
     public String toString() {
--- src/main/java/pl/com/MyDiet/MyDiet/mvc/controllers/DailySetController.java
+++ src/main/java/pl/com/MyDiet/MyDiet/mvc/controllers/DailySetController.java
@@ -1,6 +1,7 @@
 package pl.com.MyDiet.MyDiet.mvc.controllers;
 
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -25,16 +26,11 @@ import java.util.List;
 public class DailySetController {
 
     private final DailySetService dailySetService;
-    private final MealRepository mealRepository;
-    private final UserRepository userRepository;
-    private final DietConfigurator dietConfigurator;
+
 
     @Autowired
-    public DailySetController(DailySetService dailySetService, MealRepository mealRepository, UserRepository userRepository, DietConfigurator dietConfigurator) {
+    public DailySetController(DailySetService dailySetService) {
         this.dailySetService = dailySetService;
-        this.mealRepository = mealRepository;
-        this.userRepository = userRepository;
-        this.dietConfigurator = dietConfigurator;
     }
 
     @GetMapping
@@ -44,50 +40,34 @@ public class DailySetController {
         return "dailySetCreate";
     }
 
+
     @PostMapping(params = {"filter"})
     public String createDailySetPages(@ModelAttribute("dailySetDTO") DailyMealSetDTO dailySetDTO, Model model) {
+        log.info("DailySetController redirect to page with amount {}" , dailySetDTO.getMealAmount());
         dailySetDTO = dailySetService.reloadPageWithSetVariable(dailySetDTO);
         model.addAttribute("availableMeats", dailySetService.getAvailableMeats(dailySetDTO.getMealAmount()));
-        log.info("DailySetController redirect to page");
+        log.info("DailySetController redirect to page with amount {}",  dailySetDTO.getMealAmount() );
+        log.info("DailySetController redirect to page with meal size {}",  dailySetDTO.getMeals().size());
         return "dailySetCreate";
+    }
+
+    @PostMapping(params = {"modifyMealList"})
+    public String modifyMealList(@ModelAttribute("dailySetDTO") DailyMealSetDTO dailySetDTO, Model model) {
+        dailySetDTO.setMealPicked(false);
+        model.addAttribute("availableMeats", dailySetService.getAvailableMeats(dailySetDTO.getMealAmount()));
+        return "dailySetCreate";
+    }
+
+        @PostMapping(params = {"clear"})
+        public String clearForm() {
+            return "redirect:/createDailySet";
     }
 
     @PostMapping(params = {"send"})
     public String process(@ModelAttribute("dailySetDTO") DailyMealSetDTO dailySetDTO,
                           Principal principal,
                           Model model) {
-//        log.debug("DailySetController-@PostMapping(params=send): dailySetDTO ={}", dailySetDTO.toString());
-//        log.debug("DailySetController-@PostMapping(params=send): breakfastId={}, secondBreakfastId={}, dinnerId={}, teaId={}, supperId={}",
-//                breakfast, secondBreakfast, dinner, tea, supper);
-//
-//        SimpleMealsDTO simpleMealsDTO = new SimpleMealsDTO();
-//        List<Meal> meals = new ArrayList<>();
-//        if (breakfast != null) {
-//            meals.add(mealRepository.getOne(breakfast));
-//
-//        }
-//        if (secondBreakfast != null) {
-//            meals.add(mealRepository.getOne(secondBreakfast));
-//        }
-//        if (dinner != null) {
-//            meals.add(mealRepository.getOne(dinner));
-//        }
-//        if (tea != null) {
-//            meals.add(mealRepository.getOne(tea));
-//        }
-//        if (supper != null) {
-//            meals.add(mealRepository.getOne(supper));
-//        }
-//        meals.forEach(p ->{
-//            SimpleMealsDTO simpleMealsDTO = new SimpleMealsDTO();
-//            simpleMealsDTO.setId(p.getId());
-//            simpleMealsDTO.setCalories(p.getCalories());
-//            simpleMealsDTO.setName(p.getName());
-//            simpleMealsDTO.setPreparationTime(p.getPreparationTime());
-//            dailySetDTO.getListMeal().add(simpleMealsDTO);
-//            p.setCreatorUser(userRepository.findUserByUsername(principal.getName()));
-//        });
-//        log.debug("DailySetController-@PostMapping(params=send): after simpleMealsDTO setUp dailySetDTO ={}", dailySetDTO.toString());
+
         if (dailySetService.save(dailySetDTO, principal.getName())) {
             return "home-page";
         } else {
