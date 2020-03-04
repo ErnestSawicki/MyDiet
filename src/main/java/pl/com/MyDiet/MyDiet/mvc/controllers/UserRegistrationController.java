ddfe117542cb136ca4686901b85e73adbfc65854
Arkadiusz Przytuła <a.k.przytula@gmail.com>
2020-03-04 21:30:24 +0100

    new registration version 1,1

--- src/main/java/pl/com/MyDiet/MyDiet/mvc/controllers/UserRegistrationController.java
+++ src/main/java/pl/com/MyDiet/MyDiet/mvc/controllers/UserRegistrationController.java
@@ -1,24 +1,21 @@
 package pl.com.MyDiet.MyDiet.mvc.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.com.MyDiet.MyDiet.DTO.UserRegistrationDTO;
 import pl.com.MyDiet.MyDiet.beans.SecurityUtils;
-import pl.com.MyDiet.MyDiet.data.model.enumeration.Sex;
-import pl.com.MyDiet.MyDiet.data.model.User;
-import pl.com.MyDiet.MyDiet.data.repositories.UserRepository;
+
 import pl.com.MyDiet.MyDiet.services.UserService;
 
 import javax.validation.Valid;
 import java.security.Principal;
-import java.time.LocalDate;
+
 
 @Controller
 @Slf4j
@@ -32,10 +29,6 @@ public class UserRegistrationController  {
         this.userService = userService;
     }
 
-//   @GetMapping
-//   public String getUserRegistrationPage(){
-//       return "userRegistration-page";
-//   }
 
     @GetMapping
     public String getUserRegistrationPage(Model model){
@@ -44,7 +37,9 @@ public class UserRegistrationController  {
     }
 
     @PostMapping
-    public String registerUser(@Valid UserRegistrationDTO userDTO){
+    public String registerUser(@Valid UserRegistrationDTO userDTO , BindingResult bindingResult){
+        if (bindingResult.hasErrors())
+            return "user-register";
         userService.registerUser(userDTO);
         return "redirect:/";
     }
